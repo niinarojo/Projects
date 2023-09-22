5b9f5e827c3185ce6099b50880231c9f4bcc47fc
Niina Rojo <133163532+niinarojo@users.noreply.github.com>
2023-09-22 11:54:11 -0700

    Update dbo.Appointments_Update.StoredProcedure.sql

--- sql/dbo.Appointments_Update.StoredProcedure.sql
+++ sql/dbo.Appointments_Update.StoredProcedure.sql
@@ -6,7 +6,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 -- =============================================
--- Author:			  Niina Rojo
+-- Author:		Niina Rojo
 -- Create date:		07/29/2023
 -- Description:		Update proc for dbo.Appointments
 -- Code Reviewer:	Oscar
@@ -22,11 +22,11 @@ ALTER PROC [dbo].[Appointments_Update]
            ,@ClientId int
            ,@Notes nvarchar(2000) = null
            ,@LocationId int = null
-		       ,@IsConfirmed bit
+	   ,@IsConfirmed bit
            ,@AppointmentStart datetime2(7)
            ,@AppointmentEnd datetime2(7)
            ,@UserId int
-		       ,@Id int
+	   ,@Id int
 
 as
 
@@ -34,26 +34,26 @@ as
 
 Declare		@Id int = 2
 
-Declare		  @AppointmentTypeId int = 1
-			     ,@ClientId int = 1
-           ,@Notes nvarchar(2000) = 'Thest notes'
+Declare    @AppointmentTypeId int = 1
+	   ,@ClientId int = 1
+           ,@Notes nvarchar(2000) = 'Test notes'
            ,@LocationId int = 2
-		       ,@IsConfirmed bit = 1
+	   ,@IsConfirmed bit = 1
            ,@AppointmentStart datetime2(7) = '2024-06-23 08:30:20'
            ,@AppointmentEnd datetime2(7) = '2024-06-23 09:30:20'
            ,@UserId int = 4
 
 		  
 Execute [dbo].[Appointments_Update]
-		        @AppointmentTypeId
-			     ,@ClientId
+	   @AppointmentTypeId
+	   ,@ClientId
            ,@Notes
            ,@LocationId
-		       ,@IsConfirmed
+	   ,@IsConfirmed
            ,@AppointmentStart
            ,@AppointmentEnd
            ,@UserId 
-		       ,@Id
+	   ,@Id
 
 */
 
