ad4db67b5947d63a9889e9b5fc309a1759fcb2c2
Niina Rojo <133163532+niinarojo@users.noreply.github.com>
2023-09-22 11:28:59 -0700

    Create dbo.Appointments_Update.StoredProcedure.sql

---
+++ sql/dbo.Appointments_Update.StoredProcedure.sql
@@ -0,0 +1,78 @@
+USE [YellowBrick]
+GO
+/****** Object:  StoredProcedure [dbo].[Appointments_Update]    Script Date: 9/22/2023 11:27:00 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:			  Niina Rojo
+-- Create date:		07/29/2023
+-- Description:		Update proc for dbo.Appointments
+-- Code Reviewer:	Oscar
+
+-- MODIFIED BY: 
+-- MODIFIED DATE:
+-- Code Reviewer:
+-- Note:
+
+-- =============================================
+ALTER PROC [dbo].[Appointments_Update]
+           @AppointmentTypeId int
+           ,@ClientId int
+           ,@Notes nvarchar(2000) = null
+           ,@LocationId int = null
+		       ,@IsConfirmed bit
+           ,@AppointmentStart datetime2(7)
+           ,@AppointmentEnd datetime2(7)
+           ,@UserId int
+		       ,@Id int
+
+as
+
+/*
+
+Declare		@Id int = 2
+
+Declare		  @AppointmentTypeId int = 1
+			     ,@ClientId int = 1
+           ,@Notes nvarchar(2000) = 'Thest notes'
+           ,@LocationId int = 2
+		       ,@IsConfirmed bit = 1
+           ,@AppointmentStart datetime2(7) = '2024-06-23 08:30:20'
+           ,@AppointmentEnd datetime2(7) = '2024-06-23 09:30:20'
+           ,@UserId int = 4
+
+		  
+Execute [dbo].[Appointments_Update]
+		        @AppointmentTypeId
+			     ,@ClientId
+           ,@Notes
+           ,@LocationId
+		       ,@IsConfirmed
+           ,@AppointmentStart
+           ,@AppointmentEnd
+           ,@UserId 
+		       ,@Id
+
+*/
+
+
+BEGIN
+
+UPDATE [dbo].[Appointments]
+   SET [AppointmentTypeId] = @AppointmentTypeId
+      ,[ClientId] = @ClientId
+      ,[Notes] = @Notes
+      ,[LocationId] = @LocationId
+      ,[IsConfirmed] = @IsConfirmed
+      ,[AppointmentStart] = @AppointmentStart
+      ,[AppointmentEnd] = @AppointmentEnd
+      ,[DateModified] = GETUTCDATE()
+      ,[ModifiedBy] = @UserId
+ WHERE Id = @Id
+
+
+ END
+
+
